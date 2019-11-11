88498c34d00eb31a5a2419bf6f65507f98f069c8
Wassim <wassim.bekalti1@gmail.com>
2019-11-11 15:59:25 +0100

    final back

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyD0OHYj-Tv_VdiVamN3LZwpop-5uQFzL_I",
+    authDomain: "enicar-social-media.firebaseapp.com",
+    databaseURL: "https://enicar-social-media.firebaseio.com",
+    projectId: "enicar-social-media",
+    storageBucket: "enicar-social-media.appspot.com",
+    messagingSenderId: "946716093525",
+    appId: "1:946716093525:web:2d1a09ebce9d95ddde78dc"
+  };
